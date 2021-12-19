6d80ad75e19ac62af72118670c84aca5c16f0ae2
YannisNaidjaGitlab <yannis.naidja.dev@gmail.com>
2021-12-19 20:07:38 +0100

    init repo

---
+++ globalStyles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+/** global styles sheet for the app
+ * use: import styles from './globaStyles' 
+ * adapt import path if needed
+ */
+export default StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  welcome: {
+    fontSize: 20
+  }
+});
